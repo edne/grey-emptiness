7be246f3722f78bab4fca0dbc9e33244fe924d43
Edoardo Negri <edne@gmx.com>
2014-11-13 16:52:11 +0100

    cose

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-// TODO collisioni player con bolle: non deve starci SOPRA
 var W = 600;
 var H = 600;
 
@@ -10,7 +9,7 @@ function setup() {
 
     player = new Player();
 
-    for(var i=0; i<16; i++)
+    for(var i=0; i<8; i++)
         bolle[i] = new Bolla();
 
     frameRate(30);
@@ -30,6 +29,20 @@ function draw() {
     player.draw();
 }
 
+var mx, my;
+function mousePressed() {
+    mx = mouseX;
+    my = mouseY;
+}
+function mouseReleased() {
+    player.vx += 100*(mouseX - mx)/W;
+    player.vy += 100*(mouseY - my)/H;
+}
+
+function distMod(a, b, mod) {
+    return (a - b) / mod;
+}
+
 
 function Player() {
     this.x = random(W);
@@ -40,13 +53,27 @@ function Player() {
     this.vy = 0;
 
     this.update = function() {
-        for(var i=0; i<bolle.length; i++) {
-            this.vx += (this.x + this.r - bolle[i].x - bolle[i].r) / W;
-            this.vy += (this.y + this.r - bolle[i].y - bolle[i].r) / H;
-        }
+        bolle.forEach(function(b) {
+            var d = dist(this.x, this.y, b.x, b.y);
+            if(d < this.r + b.r) {
+                theta = atan2(this.y - b.y, this.x - b.x);
 
-        this.vx /= 3;
-        this.vy /= 3;
+                this.vx += min(10, this.x - b.x);
+                this.vy += min(10, this.y - b.y);
+
+                this.vx *= 0.5;
+                this.vy *= 0.5;
+
+                b.vx += b.x - this.x;
+                b.vy += b.y - this.y;
+            }
+        }, this);
+
+        this.vx *= 0.9;
+        this.vy *= 0.9;
+
+        this.vx = min(this.vx, 10);
+        this.vy = min(this.vy, 10);
 
         this.x += this.vx;
         this.y += this.vy;
@@ -68,29 +95,22 @@ function Player() {
 function Bolla() {
     this.x = random(W);
     this.y = random(H);
-    this.r = random( (W+H) ) + (W+H)>>4;
+    this.r = random( (W+H) * 2) + (W+H)>>4;
 
     this.vx = 0;
     this.vy = 0;
 
     this.update = function() {
-        for(var i=0; i<bolle.length; i++) {
-            this.vx += (this.x + this.r - bolle[i].x - bolle[i].r) / W;
-            this.vy += (this.y + this.r - bolle[i].y - bolle[i].r) / H;
-        }
+        bolle.forEach(function(b) {
+            this.vx += distMod(this.x + this.r, b.x + b.r, W);
+            this.vy += distMod(this.y + this.r, b.y + b.r, H);
+        }, this);
 
         this.vx /= 3;
         this.vy /= 3;
 
         this.x += this.vx;
         this.y += this.vy;
-
-        if(dist(player.x, player.y, this.x, this.y) < player.r+this.r) {
-            //this.vx = this.x ...
-            //player.vx += ...
-            //y
-            // e qualcosa...
-        }
 
         this.x %= W;
         this.y %= H;
